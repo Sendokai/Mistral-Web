d23319f3f95173d14c14a9da79c427c956ad239d
Sendokai <javierlago@hotmail.com>
2017-06-07 19:21:21 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+// Config DB
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', 'ascent');
+define('DB_NAME', 'auth');
+
+// Config SOAP
+define('SOAP_IP', '127.0.0.1');
+define('SOAP_PORT', '7878');
+define('SOAP_USER', 'admin');
+define('SOAP_PASS', 'admin');
+
+// Config Misc
+define('CHECK_FOR_DUPLICATE_EMAIL', true);
+define('SITE_TITLE', "Mistral Server");
+define('REALMLIST', "127.0.0.1");
---
+++ config/dbconfig.php
@@ -0,0 +1,8 @@
+﻿<?php
+$dbhost = "127.0.0.1";
+$dbuser = "root";
+$dbpass = "ascent";
+$dbname = "auth";
+$webdb = "web";
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+?>
